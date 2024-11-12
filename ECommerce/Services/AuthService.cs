ce2de66ee9ecf35403bed4573cc23bdfba8c9139
Rajsing Jadhav <rajsing.jadhav@simplifyhealthcare.com>
2024-11-12 03:24:00 +0000

    MembershioTest App Main prog written

--- ECommerce/Services/AuthService.cs
+++ ECommerce/Services/AuthService.cs
@@ -1,79 +1,120 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
-using Specification;
+//using BinaryDataRepositoryLib;
 using BinaryDataRepositoryLIb;
 using POCO;
-using System.Net;
+using Specification;
+
 namespace Services
 {
+
     public class AuthService : IAuthService
     {
+        public static string logfile = "logfile.dat";
+
+        public static string credfile = "credentials.dat";
+
         public bool Seeding()
         {
+
             bool status = false;
-            List<Credenail> credentialList = new List<Credenail>();
-            credentialList.Add(new Credenail { Email = "abcd@gmail.com", Password = "123456" });
-            credentialList.Add(new Credenail { Email = "PQRS@gmail.com", Password = "456789" });
-            List<User> userList = new List<User>();
-            userList.Add(new User { Id = 1, FirstName = "Shri", LastName = "Patil", Email = "abcd@gmail.com", ContactNo = "9527595801" });
-            userList.Add(new User { Id = 2, FirstName = "Raju", LastName = "Jadhv", Email = "pqrs@gmail.com", ContactNo = "9527595802" });
-            IDataRepository<Credenail> credentialRepository = new BinaryRepository<Credenail>();
-            IDataRepository<User> userRepository = new BinaryRepository<User>();
-            status = credentialRepository.Serialize("Credlist.dat", credentialList);
-            status = userRepository.Serialize("Userslist.dat", userList);
+            List<User> Users = new List<User>();
+            List<Credenail> credentials = new List<Credenail>();
+            Users.Add(new User { FirstName = "Rajsing", LastName = "Jadhav", Email = "Rajsing@gmail.com", ContactNo = "9860838138" });
+            Users.Add(new User { FirstName = "shri", LastName = "Patil", Email = "shri@gmail.com", ContactNo = "1234567891" });
+           
+            credentials.Add(new Credenail { Email = "ram@gmail.com", Password = "1234" });
+            credentials.Add(new Credenail { Email = "sham@gmail.com", Password = "0000" });
+            
+            IDataRepository<User> repository = new BinaryRepository<User>();
+            IDataRepository<Credenail> dataRepository = new BinaryRepository<Credenail>();
+            status = repository.Serialize(logfile, Users);
+            status = false;
+            status = dataRepository.Serialize(credfile, credentials);
             return status;
+        }
+        public bool Register(User u, string pass)
+        {
+            bool status = false;
+            List<User> users = new List<User>();
+            users = GetAllUser();
+            users.Add(u);
+            List<Credenail> credentials = new List<Credenail>();
+            credentials = GetAllCredentials();
+            Credenail credential = new Credenail { Email = u.Email, Password = pass };
+
+            credentials.Add(credential);
+            IDataRepository<User> repository = new BinaryRepository<User>();
+            status = repository.Serialize(logfile, users);
+            status = false;
+            IDataRepository<Credenail> dataRepository = new BinaryRepository<Credenail>();
+            status = dataRepository.Serialize(credfile, credentials);
+            return status;
+        }
+
+        public string ForgotPassword(string username)
+        {
+            List<Credenail> credentials = new List<Credenail>();
+            credentials = GetAllCredentials();
+            foreach (Credenail cred in credentials)
+            {
+                if (cred.Email == username)
+                {
+                    return cred.Password;
+                }
+            }
+            return null;
 
         }
-        public bool ForgotPassword(string username, string password)
+        public List<User> GetAllUser()
         {
-            throw new NotImplementedException();
+            List<User> users = new List<User>();
+            IDataRepository<User> repository = new BinaryRepository<User>();
+            users = repository.Deserialize(logfile);
+            return users;
         }
         public List<Credenail> GetAllCredentials()
         {
             List<Credenail> credentials = new List<Credenail>();
             IDataRepository<Credenail> repository = new BinaryRepository<Credenail>();
-            credentials = repository.Deserialize("Credlist.dat");
+            credentials = repository.Deserialize(credfile);
             return credentials;
         }
-        public List<User> GetAllUsers()
-        {
-            List<User> users = new List<User>();
-            IDataRepository<User> repository = new BinaryRepository<User>();
-            users = repository.Deserialize("Userslist.dat");
-            return users;
-        }
+
 
         public bool Login(string username, string password)
         {
-            throw new NotImplementedException();
+            IDataRepository<Credenail> repository = new BinaryRepository<Credenail>();
+            List<Credenail> credentials = repository.Deserialize(credfile);
+            foreach (Credenail cred in credentials)
+            {
+                if (cred.Email == username && cred.Password == password)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
 
-        public bool Register(User u, string pwd)
+        public bool ResetPassword(string username, string oldpassword, string newpassword)
         {
-            bool status = false;
-            List<User> users = new List<User>();
-            users = GetAllUsers();
-            users.Add(u);
             List<Credenail> credentials = new List<Credenail>();
             credentials = GetAllCredentials();
-            Credenail credential = new Credenail { Email = u.Email, Password = pwd };
-            credentials.Add(credential);
-
-            IDataRepository<User> repository = new BinaryRepository<User>();
-            status = repository.Serialize("Userslist.dat", users);
-
-            IDataRepository<Credenail> dataRepository = new BinaryRepository<Credenail>();
-            status = dataRepository.Serialize("Credlist.dat", credentials);
-            return status;
-            // throw new NotImplementedException();
-        }
-
-        public bool ResetPassword(string username, string oldPassword, string newPassword)
-        {
-            throw new NotImplementedException();
+            foreach (Credenail cred in credentials)
+            {
+                if (cred.Email == username & cred.Password == oldpassword)
+                {
+                    cred.Password = newpassword;
+                    IDataRepository<Credenail> dataRepository = new BinaryRepository<Credenail>();
+                    return dataRepository.Serialize(credfile, credentials);
+                }
+            }
+            return false;
         }
     }
 }
--- ECommerce/Specification/IAuthService.cs
+++ ECommerce/Specification/IAuthService.cs
@@ -1,21 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using POCO;
 
 namespace Specification
 {
-    public  interface IAuthService
+    public interface IAuthService
     {
-
         bool Login(string username, string password);
-        bool Register(User user,string password);
-        bool ForgotPassword(string username, string password);
-        bool ResetPassword(string username, string oldPassword, string newPassword);
+        bool Register(User user, string password);
 
-        List<User> GetAllUsers();  
-        List<Credenail> GetAllCredentials();    
+        string ForgotPassword(string username);
+
+        bool ResetPassword(string username, string oldpassword, string newpassword);
+
+        List<User> GetAllUser();
+        List<Credenail> GetAllCredentials();
     }
 }
